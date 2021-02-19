4581efc9150ae661c2d3a8f75ea4b4e6970ff178
ahernandez1200 <tony.6.h@gmail.com>
2021-02-19 15:38:21 -0800

    Display book info on webpage

--- public/book.js
+++ public/book.js
@@ -1,18 +1,48 @@
 
 $(document).ready(function(){
 
-   alert("hello");
-   var getReqURL =
-      "https://www.googleapis.com/books/v1/volumes?q=From_Here_to_eternity&maxResults=1&key=AIzaSyCg_gMcW0yKtYA4T03lt2sWcBbAz5581rc";
+var getReqURL =
+   "https://www.googleapis.com/books/v1/volumes?q=From_Here_to_eternity&maxResults=1&key=AIzaSyCg_gMcW0yKtYA4T03lt2sWcBbAz5581rc";
 
 
-   fetch(getReqURL)
-   .then( (response)=>response.json())
-   .then( (data)=>{
-      alert(data.items[0].volumeInfo.description);
-   })
+fetch(getReqURL)
+.then( (response)=>response.json())
+.then( (data)=>{
+     // alert(data.items[0].volumeInfo.description);
+     addBookTitle(data);
+     addCoverImg(data);
+     addAuthor(data);
+     addDescription(data);
+});
 
+
+function addBookTitle(jsonObj) {
+   let newP = document.createElement("p");
+   newP.innerHTML ="<em>" + jsonObj.items[0].volumeInfo.title + "</em>";
+   document.getElementById('bookContent').appendChild(newP);
+}
+
+function addCoverImg(jsonObj) {
+   let newImg = document.createElement("img");
+   newImg.src = jsonObj.items[0].volumeInfo.imageLinks.thumbnail;
+   document.getElementById('bookContent').appendChild(newImg);
+}
+
+function addAuthor(jsonObj) {
+   let newP = document.createElement("p");
+   newP.innerHTML ="<strong>" + jsonObj.items[0].volumeInfo.authors + "</strong>";
+   document.getElementById('bookContent').appendChild(newP);
+}
+
+function addDescription(jsonObj) {
+   let newP = document.createElement("p");
+   newP.innerHTML = jsonObj.items[0].volumeInfo.description;
+   document.getElementById('bookContent').appendChild(newP);
+
+}
 
 
 
 });
+
+//data.items[0].volumeInfo.description
