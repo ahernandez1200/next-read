d4e677a1ad94855d36f430c8dfbfa1b7348a450a
ahernandez1200 <tony.6.h@gmail.com>
2021-02-18 18:43:24 -0800

    testing fetch

--- public/book.js
+++ public/book.js
@@ -1,4 +1,18 @@
 
 $(document).ready(function(){
+
    alert("hello");
+   var getReqURL =
+      "https://www.googleapis.com/books/v1/volumes?q=From_Here_to_eternity&maxResults=1&key=AIzaSyCg_gMcW0yKtYA4T03lt2sWcBbAz5581rc";
+
+
+   fetch(getReqURL)
+   .then( (response)=>response.json())
+   .then( (data)=>{
+      alert(data.items[0].volumeInfo.description);
+   })
+
+
+
+
 });
