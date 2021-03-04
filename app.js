dd1a9b6f4d7503209575c1ac0d5b2e28d71ef8c8
ahernandez1200 <tony.6.h@gmail.com>
2021-03-03 18:21:06 -0800

    implement file reading

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 
-
 // require('dotenv').config();
 
 //creating principal applications
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const fetch = require("node-fetch");
+// const fetch = require("node-fetch");
+const fs = require('fs');
 const app = express();
+
+
+//reading the contents of the .txt files within bookScrapper/ into variables
+const nbtFiction = fs.readFileSync("bookScrapper/natBookTitlesFiction.txt",'utf8');
+const nbtNonFiction = fs.readFileSync("bookScrapper/natBookTitlesNonFiction.txt",'utf8');
+const nbtNonPoetry = fs.readFileSync("bookScrapper/natBookTitlesPoetry.txt",'utf8');
+const pptFiction = fs.readFileSync("bookScrapper/pulPrizeTitlesFiction.txt",'utf8');
+const pptNonFiction = fs.readFileSync("bookScrapper/pulPrizeTitlesNonFiction.txt",'utf8');
+const pptNonPoetry = fs.readFileSync("bookScrapper/pulPrizeTitlesPoetry.txt",'utf8');
 
 
 //specifies the directory from which to serve static files
@@ -18,14 +27,32 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+//starting the server
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
 
+//home directory
 app.get("/", (req, res)=>{
    res.render("home");
-
 });
+
+
+// function convertFileContentToString(path) {
+//
+//    someVar = fs.readFileSync(path,'utf8');
+//    console.log(someVar);
+//
+//    //asychrnouns
+//    // fs.readFile(path, 'utf8', function(err, data) {
+//    //     if (err) throw err;
+//    //     var content = data.toString();
+//    //     setSomeVar(content);
+//    // }) ;
+//
+// }
+
+
 
 
 // var currentGetRequest =
