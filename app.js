c4fa213ee22a177581ed2e25c4606b0f6504908f
ahernandez1200 <53980216+ahernandez1200@users.noreply.github.com>
2021-04-06 14:19:55 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,101 +1,225 @@
 
-// require('dotenv').config();
+$(document).ready(function(){
 
-//creating principal applications
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
-// const fetch = require("node-fetch");
-const fs = require('fs');
-const app = express();
+$(".cat-alert-1").hide().fadeIn(1000).fadeOut(1000);
+
+$("#nbtNonFiction").text();
+
+/*we will append elements containing book-related info to the div with this
+  ID*/
+const divID = "bookDetails";
+const dfltBook="The_Known_World";
+
+let natFicBooks = $("#nbtFiction").text().split("|||");
+let natNonFicBooks =  $("#nbtNonFiction").text().split("|||");
+let natPoetryBooks =  $("#nbtPoetry").text().split("|||");
+let pulFicBooks =  $("#pptFiction").text().split("|||");
+let pulNonFicBooks =  $("#pptNonFiction").text().split("|||");
+let pulPoetryBooks =  $("#pptPoetry").text().split("|||");
+
+let previousBookTitle="";
+let currentBookTitle = dfltBook;
+
+var bookReqUrl =
+            "https://www.googleapis.com/books/v1/volumes?q=" +currentBookTitle+ "&maxResults=1&key=AIzaSyCg_gMcW0yKtYA4T03lt2sWcBbAz5581rc";
+
+/*this var is updated whenever the user selects a category from the drop-down
+  menu. The id of the selected dropdown-item is recorded in this var.*/
+let currentCategory = "";
+
+let tempCurrentCategory = "";
 
 
-//reading the contents of the .txt files within bookScrapper/ into variables
-const nbtFiction = fs.readFileSync("bookScrapper/natBookTitlesFiction.txt",'utf8');
-const nbtNonFiction = fs.readFileSync("bookScrapper/natBookTitlesNonFiction.txt",'utf8');
-const nbtPoetry = fs.readFileSync("bookScrapper/natBookTitlesPoetry.txt",'utf8');
-const pptFiction = fs.readFileSync("bookScrapper/pulPrizeTitlesFiction.txt",'utf8');
-const pptNonFiction = fs.readFileSync("bookScrapper/pulPrizeTitlesNonFiction.txt",'utf8');
-const pptPoetry = fs.readFileSync("bookScrapper/pulPrizeTitlesPoetry.txt",'utf8');
+//displaying the book info of the default book.
+displayBookAndInfo(bookReqUrl);
+
+//adding click event to the book categories
+$(".dropdown-item").click(function(){
+   // $("#categoryAlert").fadeIn(1500).fadeOut(1500);
+   $(".cat-alert-2").hide().fadeIn(750).fadeOut(750);
 
 
-
-//specifies the directory from which to serve static files
-app.use(express.static("public"));
-//enabling the use of static template files
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
-//starting the server
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+   //recording the id of the selected category
+   currentCategory = $(this).attr('id');
+   newBookReqUrl();
 });
 
-//home directory
-app.get("/", (req, res)=>{
-   res.render("home",
-      {  nbtFiction: nbtFiction,
-         nbtNonFiction: nbtNonFiction,
-         nbtPoetry: nbtPoetry,
-         pptFiction: pptFiction,
-         pptNonFiction: pptNonFiction,
-         pptPoetry: pptPoetry
-      }
+function newBookReqUrl() {
+   let bookTitle = "";
+   tempCurrentCategory = currentCategory;
 
-   );
+   if(tempCurrentCategory == "NAT" || tempCurrentCategory == "PUL") {
+      tempCurrentCategory = tempCurrentCategory +
+         (1 + (Math.floor(Math.random()*100) % 3) ).toString();
+   }
+   if(tempCurrentCategory == "NAT1") {
+      previousBookTitle = currentBookTitle
+      currentBookTitle =
+         natFicBooks[ Math.floor(Math.random()*100) % natFicBooks.length];
+
+      bookReqUrl = bookReqUrl.replace(previousBookTitle, currentBookTitle);
+   }
+   else if(tempCurrentCategory == "NAT2") {
+      previousBookTitle = currentBookTitle
+      currentBookTitle =
+         natFicBooks[ Math.floor(Math.random()*100) % natFicBooks.length];
+
+      bookReqUrl = bookReqUrl.replace(previousBookTitle, currentBookTitle);
+   }
+   else if(tempCurrentCategory == "NAT3") {
+      previousBookTitle = currentBookTitle
+      currentBookTitle =
+         natFicBooks[ Math.floor(Math.random()*100) % natFicBooks.length];
+
+      bookReqUrl = bookReqUrl.replace(previousBookTitle, currentBookTitle);
+   }
+   else if(tempCurrentCategory == "PUL1") {
+      previousBookTitle = currentBookTitle
+      currentBookTitle =
+         natFicBooks[ Math.floor(Math.random()*100) % natFicBooks.length];
+
+      bookReqUrl = bookReqUrl.replace(previousBookTitle, currentBookTitle);
+   }
+   else if(tempCurrentCategory == "PUL2") {
+      previousBookTitle = currentBookTitle
+      currentBookTitle =
+         natFicBooks[ Math.floor(Math.random()*100) % natFicBooks.length];
+
+      bookReqUrl = bookReqUrl.replace(previousBookTitle, currentBookTitle);
+   }
+   else if(tempCurrentCategory == "PUL3") {
+      previousBookTitle = currentBookTitle
+      currentBookTitle =
+         natFicBooks[ Math.floor(Math.random()*100) % natFicBooks.length];
+
+      bookReqUrl = bookReqUrl.replace(previousBookTitle, currentBookTitle);
+   }
+}
+
+$(".bcButton").click(function(){
+
+   $( "#" + divID ).remove();
+   newBookReqUrl();
+   displayBookAndInfo(bookReqUrl);
 });
 
-app.get("/about", (req, res)=>{
-   res.render("about");
+
+function displayBookAndInfo(bookGetReq) {
+   fetch(bookGetReq)
+   .then( (response)=>response.json())
+   .then( (data)=>{
+        // alert(data.items[0].volumeInfo.description);
+        addDiv();
+        addBookTitle(data);
+        addCoverImg(data);
+        addAuthor(data);
+        addCategory();
+        addDescription(data);
+        addPublishedDate(data);
+   });
+}
+
+/*Elements that contain book-related information will be appended as children
+  the div created in this function. The reason why we add this div in the
+  first place is so that when we want to display the info of a new book,
+   we can delete this div and that in turn will delete all the info
+   related to the previous book that was displayed.
+*/
+function addDiv(){
+   let newDiv = document.createElement("div");
+   newDiv.id = divID;
+   $(newDiv).insertBefore(".bcButton");
+
+}
+
+function addBookTitle(jsonObj) {
+   let newP = document.createElement("p");
+   newP.innerHTML ="<em>" + jsonObj.items[0].volumeInfo.title + "</em>";
+   // document.getElementById('bookContent').appendChild(newP);
+   // $(newP).insertBefore(".bcButton");
+   document.getElementById(divID).appendChild(newP);
+
+}
+
+function addCategory() {
+   let newPAward = document.createElement("p");
+   let newPCategory = document.createElement("p");
+   newPCategory.innerHTML="";
+
+   // alert("in add category..tempCurrentCategory: " + tempCurrentCategory);
+   if(tempCurrentCategory == "NAT1") {
+      newPAward.innerHTML = "National Book Award";
+      newPCategory.innerHTML = "Fiction";
+   }
+   else if(tempCurrentCategory == "NAT2") {
+      newPAward.innerHTML = "National Book Award";
+      newPCategory.innerHTML = "Non-Fiction";
+   }
+   else if(tempCurrentCategory == "NAT3") {
+      newPAward.innerHTML = "National Book Award";
+      newPCategory.innerHTML = "Poetry";
+   }
+   else if(tempCurrentCategory == "PUL1") {
+      newPAward.innerHTML = "Pulitzer Prize";
+      newPCategory.innerHTML = "Fiction";
+   }
+   else if(tempCurrentCategory == "PUL2") {
+      newPAward.innerHTML = "Pulitzer Prize";
+      newPCategory.innerHTML = "General Non-Fiction";
+   }
+   else  {
+      newPAward.innerHTML = "Pulitzer Prize";
+      newPCategory.innerHTML = "Poetry";
+   }
+
+   document.getElementById(divID).appendChild(newPAward);
+
+   if(newPCategory.innerHTML != "")
+      document.getElementById(divID).appendChild(newPCategory);
+}
+
+function addCoverImg(jsonObj) {
+   let newImg = document.createElement("img");
+   newImg.src = jsonObj.items[0].volumeInfo.imageLinks.thumbnail;
+   // document.getElementById('bookContent').appendChild(newImg);S
+   // $(newImg).insertBefore(".bcButton");
+   document.getElementById(divID).appendChild(newImg);
+
+}
+
+function addAuthor(jsonObj) {
+   let newP = document.createElement("p");
+   newP.innerHTML ="<strong>" + jsonObj.items[0].volumeInfo.authors + "</strong>";
+   // document.getElementById('bookContent').appendChild(newP);
+   // $(newP).insertBefore(".bcButton");
+   document.getElementById(divID).appendChild(newP);
+
+}
+
+function addDescription(jsonObj) {
+   let newP = document.createElement("p");
+   newP.innerHTML = jsonObj.items[0].volumeInfo.description.substring(0, 550);
+
+   if(newP.innerHTML.length >= 550)
+      newP.innerHTML += " . . . ";
+
+   // document.getElementById('bookContent').appendChild(newP);
+   // $(newP).insertBefore(".bcButton");
+   document.getElementById(divID).appendChild(newP);
+
+
+}
+
+function addPublishedDate(jsonObj) {
+   let newP = document.createElement("p");
+   newP.innerHTML = jsonObj.items[0].volumeInfo.publishedDate;
+   // document.getElementById('bookContent').appendChild(newP);
+   // $(newP).insertBefore(".bcButton");
+   document.getElementById(divID).appendChild(newP);
+
+
+}
+
+
+
 });
-
-// function convertFileContentToString(path) {
-//
-//    someVar = fs.readFileSync(path,'utf8');
-//    console.log(someVar);
-//
-//    //asychrnouns
-//    // fs.readFile(path, 'utf8', function(err, data) {
-//    //     if (err) throw err;
-//    //     var content = data.toString();
-//    //     setSomeVar(content);
-//    // }) ;
-//
-// }
-
-
-
-
-// var currentGetRequest =
-//    "https://www.googleapis.com/books/v1/volumes?q=From_Here_to_eternity&maxResults=1&key=AIzaSyCg_gMcW0yKtYA4T03lt2sWcBbAz5581rc";
-
-
-// function temp() {
-// fetch(currentGetRequest)
-//   .then(response => response.json())
-//   .then(data => {
-//      // console.log(data.items[0].volumeInfo.title);
-//      addBookChild(data);
-//      // throw new Error('Exiting function.');
-//   });
-//   // .catch(error=> console.log("exit."));
-// }
-//
-//   function addBookChild(googleRetObj) {
-//
-   //   $(document).ready(function(){
-   //      const newImg = $(document).createElement("img");
-   //      const brk = document.createElement("br");
-   //      newImg.src = googleRetObj.items[0].volumeInfo.imageLinks.smallThumbnail;
-   //      document.getElementById('body').appendChild(newImg);
-   //      document.getElementById('body').appendChild(brk);
-   // });
-//
-//
-//  };
-
-// $(document).ready(function(){
-//    alert("hello");
-// }
